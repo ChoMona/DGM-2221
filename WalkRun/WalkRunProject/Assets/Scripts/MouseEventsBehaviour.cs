02ab2b53cf7509088fca1fafc751f4da660dd49f
Paloma <paloma98dasilva@gmail.com>
2020-11-06 21:17:43 -0700

    nightLight environment

---
+++ WalkRun/WalkRunProject/Assets/Scripts/MouseEventsBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseEventsBehaviour : MonoBehaviour
+{
+    public UnityEvent mouseDownEvent;
+    
+    private void OnMouseDown()
+    {
+        mouseDownEvent.Invoke();
+    }
+}
